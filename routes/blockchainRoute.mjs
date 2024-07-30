773552d23e6c2e5fdd057a2c221359732d51f12a
TenTimothy <saba@wmooragency.co>
2024-07-30 14:07:27 +0200

    Router fix

---
+++ routes/blockchainRoute.mjs
@@ -0,0 +1,5 @@
+import { Router } from 'express'; 
+
+const blockchainRouter = Router();
+
+export default blockchainRouter;
--- server.mjs
+++ server.mjs
@@ -1,7 +1,13 @@
 import express from 'express';
+import blockchainRouter from './routes/blockchainRoute.mjs';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/blockchain', blockchainRouter);
 
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Nu är server ${PORT} igång`));
+
